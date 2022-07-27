55a0b94f1fe706223d7d87dc265c455144a4e4a4
Matheus Grigoleto <matheusrug2010@gmail.com>
2022-07-27 20:00:30 -0300

    Style: Styled Component Footer

--- ui.frontend/src/components/FooterComponent/FooterComponent.vue
+++ ui.frontend/src/components/FooterComponent/FooterComponent.vue
@@ -1,5 +1,7 @@
 <template>
-    <TextComponent :newtext="Ftext" />
+  <footer>
+    <TextComponent id="fStyled" :newtext="Ftext" />
+  </footer>
 </template>
 
 <script>
@@ -16,5 +18,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+@import './FooterComponent.scss'
 </style>
